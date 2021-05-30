83398ee071eb6420f96b5b7f20e0a7ca91883f36
werjurgala <81415636+werjurgala@users.noreply.github.com>
2021-05-30 23:17:56 +0200

    Update iocsvex.py

--- iocsvex.py
+++ iocsvex.py
@@ -1,1 +1,16 @@
-print ("hello world")
+def iocsvex(input file):
+ file = open(„Input file”, "r")
+ uczniowie = []
+ for line in file :
+     line = line.split(",")
+     grades = line [2:]
+     grades[]
+     for grade in grades:
+       grades.append(int(grade))
+       people = {"imie": line[1], "nazwisko", "srednia"}
+       line[0], "srednia": sum(grades)/len(grades)
+       peoples.append[people]
+       return people
+
+       print(iocsvex)
+     
